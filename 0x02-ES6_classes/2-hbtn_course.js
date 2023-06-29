41394caa920b48f6ff16ce78b0b65fdfba5888e7
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-06-29 22:43:53 +0000

    getter setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+// Implements a class named HolbertonCourse
+
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+    // get the name of the classroom
+    get name() {
+        return this._name;
+    }
+    // set the name of the classroom
+    set name(name) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string')
+        }
+        this._name = name;
+    }
+
+    // get the length of the classroom
+    get length() {
+        return this._length;
+    }
+    // set the length of the classroom
+    set length(length) {
+        if (typeof length !== 'number') {
+            throw new TypeError('Length must be a number')
+        }
+        this._length = length;
+    }
+
+    // get the students of the classroom
+    get students() {
+        return this._students;
+    }
+    // set the students of the classroom
+    set students(students) {
+        if (typeof students !== 'object') {
+            throw new TypeError('students must be an array')
+        }
+        this._students = students;
+    }
+
+}
