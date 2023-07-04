e3fced98780c07ee91ce2f0b52e71e5fcf3d48eb
Ovy Evbodi <96619760+OvyEvbodi@users.noreply.github.com>
2023-07-04 23:09:57 +0000

    Typed arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+const createInt8TypedArray = (length, position, value) => {
+    const buffer = new ArrayBuffer(length);
+    if (0 <= position < length) {
+        const dv = new DataView(buffer);
+        dv.setInt8(position, value)
+        return dv;
+    }
+     throw new Error('Position outside range');   
+};
+
+export default createInt8TypedArray;
